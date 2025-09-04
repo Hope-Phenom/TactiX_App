b1e4004b3cbeb8242fada03a52532c42e771d2fa
Hope.Phenom <hope.phenom@foxmail.com>
2025-09-05 00:24:57 +0800

    [功能] 1. 添加信息展示弹窗 2. 主窗体修改为不规则窗体

--- TactiX_App/Views/MainWindow.axaml.cs
+++ TactiX_App/Views/MainWindow.axaml.cs
@@ -1,11 +1,22 @@
 ﻿using Avalonia.Controls;
 
+using TactiX_OS_Tools;
+
 namespace TactiX_App.Views;
 
 public partial class MainWindow : Window
 {
+    #region 变量
+    private IOSes OSes;
+    #endregion
+
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    public void Init()
+    {
+
     }
 }
---
+++ TactiX_App/Views/Popup/ErrorPopup.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace TactiX_App;
+
+public partial class ErrorPopup : Window
+{
+    public ErrorPopup()
+    {
+        InitializeComponent();
+    }
+}
