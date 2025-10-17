93a67330c4b50653006e91eae9d9bfb94d3669a9
Hope.Phenom <hope.phenom@foxmail.com>
2025-10-17 15:10:21 +0800

    [功能] 1. 添加TitleBar图片的展示

--- TactiX_App/ViewModels/Popup/TacticPlayWindowModel.cs
+++ TactiX_App/ViewModels/Popup/TacticPlayWindowModel.cs
@@ -1,4 +1,6 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Media;
+using Avalonia.Media.Imaging;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
@@ -7,12 +9,16 @@ using NLog;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TactiX_I18N;
 using TactiX_Logger;
+using TactiX_Models;
 using TactiX_Models.MessageBus;
+using TactiX_ModSupport;
+using TactiX_OS_Tools;
 
 namespace TactiX_App.ViewModels.Popup
 {
@@ -22,14 +28,24 @@ namespace TactiX_App.ViewModels.Popup
         public ILanguage Language { get; private set; }
         private readonly Logger _logger;
         private readonly IMessenger _messenger;
+        private readonly L_Config _config;
+        private readonly IOSes _oses;
         #endregion
 
         #region 变量/常量
         private const string WINDOW_NAME = "TacticPlayWindow";
+        private const string TITLE_BAR_IMAGE = "titlebar.png";
         private readonly Point PLAYING_SIZE = new(700, 180);
         private readonly Point MINI_SIZE = new(700, 230);
         private readonly Point NORMAL_SIZE = new(700, 500);
 
+        /// <summary>
+        /// 当前装载的Mod
+        /// </summary>
+        private readonly ModPackage _modPackage;
+        #endregion
+
+        #region 数据绑定
         /// <summary>
         /// UI是否是准备模式
         /// </summary>
@@ -58,20 +74,34 @@ namespace TactiX_App.ViewModels.Popup
         /// </summary>
         [ObservableProperty]
         public MaterialIconKind materialIconKind;
+        /// <summary>
+        /// TitleBarImage
+        /// </summary>
+        [ObservableProperty]
+        public IImage titleBarImage;
         #endregion
 
-        public TacticPlayWindowModel(ILang lang, ILoggerContainer loggerContainer, IMessenger messenger) 
+        public TacticPlayWindowModel(ILang lang, ILoggerContainer loggerContainer, IMessenger messenger, 
+            IOSTools oSTools) 
         {
             Language = lang.Language;
             _logger = loggerContainer.Builder.GetCurrentClassLogger();
             _messenger = messenger;
+            _oses = oSTools.OSes;
+            _config = _oses.LoadConfig();
 
+            // 加载指定MOD
+            _modPackage = new ModPackage(_config.CurrentlyEnabledMOD);
+
+            // UI初始化
+            IsPrepare = true;
             UIHeight = NORMAL_SIZE.Y;
-
             HorizontalLineHeight = new GridLength(10);
             GroupHeight = GridLength.Star;
-
             MaterialIconKind = MaterialIconKind.ArrowExpandUp;
+
+            using var ms = new MemoryStream(_modPackage.ReadBinaryFile(TITLE_BAR_IMAGE));
+            titleBarImage = new Bitmap(ms);
         }
 
 #if DEBUG
@@ -90,6 +120,8 @@ namespace TactiX_App.ViewModels.Popup
             { 
                 Name = WINDOW_NAME
             });
+
+            _modPackage.Dispose();
         }
 
         /// <summary>
