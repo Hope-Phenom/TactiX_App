d9542d865f5e396a6d8b528e8f7d39dd5cab4e3e
Hope.Phenom <hope.phenom@foxmail.com>
2025-10-22 15:47:42 +0800

    [功能] 1. 导入AvaloniaEdit作为文本编辑的基础组件

--- TactiX_App/ViewModels/Page/TacticEditorPageViewModel.cs
+++ TactiX_App/ViewModels/Page/TacticEditorPageViewModel.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.Messaging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TactiX_I18N;
 
 namespace TactiX_App.ViewModels.Page
 {
     public class TacticEditorPageViewModel : ViewModelBase
     {
+        #region DI容器注入
+        private readonly ILanguage _language;
+        private readonly IMessenger _messenger;
+        #endregion
+
+        #region 数据绑定
+        public ILanguage Language => _language;
+        #endregion
+
+        public TacticEditorPageViewModel(IMessenger messenger, ILang lang)
+        {
+            _language = lang.Language;
+            _messenger = messenger;
+        }
     }
 }
---
+++ TactiX_Models/MessageBus/MB_WindowTitle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TactiX_Models.MessageBus
+{
+    /// <summary>
+    /// 通知消息，通知对应的Window修改标题文本
+    /// </summary>
+    public class MB_WindowTitle
+    {
+        public required string Title { get; set; }
+        public required string WindowName { get; set; }
+    }
+}
